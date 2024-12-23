800c96117701942bfedd992be8afdb8c3c0ec0a6
Cola Chan <57797863+141forever@users.noreply.github.com>
2024-12-23 20:55:08 +0800

    Update generate_final_sample2.py

--- generate_final_sample2.py
+++ generate_final_sample2.py
@@ -1,3 +1,96 @@
+def get_wrong_reason(idx,path,now_problem_info,problem_id_now,input_format_requirement,explanation11,explanation22,max_x):
+    result = subprocess.run(['python3', './problems/' + problem_id_now + '/' + 'judge.py','--code_path','code_fan_'+str(idx)+'.py'], capture_output=True,text=True)
+    time.sleep(3)
+    with open(path + "/first_python_results.json", "r", encoding="utf-8") as json_file:
+        now_first_result = json.load(json_file)
+    error_message = now_first_result['message']
+    match = re.search(r'样例：\n(.*?)\n', error_message)
+    try:
+        input3 = int(match.group(1))
+    except:
+        input3 = "There are no specific sample inputs, and the program itself has an error:" +error_message
+    output3 = ""
+    if input3 == "There are no specific sample inputs, and the program itself has an error:" +error_message:
+        output3 = "There are no specific sample outputs"
+    else:
+        for i in range(len(now_problem_info['test'])):
+            tmp_d = now_problem_info['test'][i]
+            if tmp_d['input'] == input3:
+                output3 = tmp_d['output']
+                break
+    try:
+        assert output3 != ""
+    except:
+        pdb.set_trace()
+
+    code_now = ""
+    with open(path + '/code_fan_' + str(idx) + '.py', 'r', encoding='utf-8') as file:
+        code_now = file.read()
+    assert code_now != ""
+
+    if idx != 0:
+        code_next = ""
+        with open(path + '/code_fan_' + str(idx-1) + '.py', 'r', encoding='utf-8') as file:
+            code_next = file.read()
+        assert code_next != ""
+    else:
+        code_next = ""
+        with open(path + '/code.py', 'r', encoding='utf-8') as file:
+            code_next = file.read()
+        assert code_next != ""
+
+
+    if idx > 1: 
+        cnt_try = 0
+        f = 1
+        while True:
+            if cnt_try > 1:
+                f = 0
+                break
+            try:
+                cnt_try += 1
+                prompt3 = copy.deepcopy(gaixie_code).format(origin_code=code_next,
+                                                            json_format='''{"function":"xxx","rewrite_code":"xxx"}''')
+                code_next = generate_one(prompt3, temp=1, model_name='deepseek-chat')
+                code_next = code_next[code_next.find('{'):code_next.rfind('}') + 1]
+                code_next = json.loads(code_next)['rewrite_code']
+                break
+            except:
+                continue
+
+    prompt1 = copy.deepcopy(python_error_prompt)
+    prompt1 = prompt1.format(problem=now_problem_info['description'],
+                            input_format=input_format_requirement,
+                            output_format=now_problem_info['output requirement'],
+                            input1=now_problem_info['example'][0]['input'],
+                            input2=now_problem_info['example'][1]['input'],
+                            example_reason1=explanation11,
+                            output1=now_problem_info['example'][0]['output'],
+                            output2=now_problem_info['example'][1]['output'],
+                            example_reason2=explanation22,
+                            code=code_now,
+                            input3=input3,
+                            output3=output3,
+                            code2=code_next,
+                            json_type='''{"work_reason":xxx,"wrong_reason":xxx}''',
+                            ).strip()
+    cnt_try = 0
+    f = 1
+    while True:
+        if cnt_try > 1:
+            f = 0
+            break
+        try:
+            cnt_try += 1
+            pre_analyse = generate_one(prompt1, temp=1, model_name='deepseek-chat')
+            pre_analyse = pre_analyse[pre_analyse.find('{'):pre_analyse.rfind('}') + 1]
+            pre_analyse = json.loads(pre_analyse)
+            break
+        except:
+            continue
+    assert 'work_reason' and 'wrong_reason' in pre_analyse.keys()
+    return input3,output3,pre_analyse,code_next
+
 def generate_final_sample(vis,target_folders,origin_data_dict):
     for path in target_folders:
         problem_id_now = path[path.rfind('/') + 1:]
